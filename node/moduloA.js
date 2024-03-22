adff76db3470dabaee706feb110b00eaad226854
RobsonMendes37 <robsonqueirozmendes@alu.ufc.br>
2024-03-22 15:06:51 -0300

    iniciando o modulo node

---
+++ node/moduloA.js
@@ -0,0 +1,8 @@
+this.ola ='ola'
+exports.bemVindo = 'Seja bem vindo'
+module.exports.ateLogo = 'Ate a aproxima'
+
+//coloquei atributos diferentes para o objeto
+//this
+
+console.log(this)
---
+++ node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,4 @@
+const moduloA=require('../../moduloA')
+console.log(moduloA.ola)
+
+//importando o moduloA de duas pastas antes
